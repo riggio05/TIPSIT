2fb7f55d88626b3dd7932d785b263899bcc6c080
riggio05 <simone.riggio@itiszuccante.edu.it>
2023-03-29 08:17:43 +0200

    semafori

--- ES13/src/Main.java
+++ ES13/src/Main.java
@@ -7,14 +7,23 @@ public class Main {
         Concorrenza t1 = new Concorrenza("t1", cont);
         Concorrenza t2 = new Concorrenza("t2", cont);
         Concorrenza t3 = new Concorrenza("t3", cont);
+        Concorrenza t4 = new Concorrenza("t4", cont);
+        Concorrenza t5 = new Concorrenza("t5", cont);
+        Concorrenza t6 = new Concorrenza("t6", cont);
         t1.start();
         t2.start();
         t3.start();
+        t4.start();
+        t5.start();
+        t6.start();
         //aspetto prima che i thread finiscano
         try {
             t1.join();
             t2.join();
             t3.join();
+            t4.join();
+            t5.join();
+            t6.join();
         } catch (Exception e) {}
         System.out.println("valore finale del contatore " + cont.cont);
         System.out.println("fine del processo");
