cbeab2bfc89215d72906ad610626f4b7a021937a
riggio05 <simone.riggio@itiszuccante.edu.it>
2023-05-10 10:34:44 +0200

    siste

--- LineaFerroviaria/src/Main.java
+++ LineaFerroviaria/src/Main.java
@@ -1,11 +1,16 @@
 import java.util.concurrent.Semaphore;
-
 public class Main {
-
     public static void main(String[] args) {
-        Semaphore binario=new Semaphore(1);
-        Treno t1=new Treno(1,2,binario);
-        Thread thread1=new Thread(t1);
+        Semaphore scambio1 = new Semaphore(1);
+        Semaphore scambio2 = new Semaphore(1);
+        Semaphore stazione1 = new Semaphore(1);
+        Semaphore stazione2 = new Semaphore(1);
+        Semaphore stazione3 = new Semaphore(1);
+        Treno t1 = new Treno(1, 2, scambio1, stazione1, stazione2);
+        Treno t2 = new Treno(2, 3, scambio2, stazione2, stazione3);
+        Thread thread1 = new Thread(t1);
+        Thread thread2 = new Thread(t2);
         thread1.start();
+        thread2.start();
     }
 }
--- LineaFerroviaria/src/Treno.java
+++ LineaFerroviaria/src/Treno.java
@@ -1,24 +1,43 @@
 import java.util.concurrent.Semaphore;
 
-public class Treno implements Runnable{
+public class Treno implements Runnable {
     private int stPartenza;
     private int stArrivo;
-    private Semaphore s; //binario che collega le due stazioni
-    public Treno(int stPartenza, int stArrivo, Semaphore s){
-        this.stPartenza=stPartenza;
-        this.stArrivo=stArrivo;
-        this.s=s;
+    private Semaphore scambio;
+    private Semaphore stazionePartenza;
+    private Semaphore stazioneArrivo;
+    public Treno(int stPartenza, int stArrivo, Semaphore scambio, Semaphore stazionePartenza, Semaphore stazioneArrivo) {
+        this.stPartenza = stPartenza;
+        this.stArrivo = stArrivo;
+        this.scambio = scambio;
+        this.stazionePartenza = stazionePartenza;
+        this.stazioneArrivo = stazioneArrivo;
     }
-    public void run(){
-        while(true){
-            System.out.println("La partenza è prevista dalla stazione "+stPartenza);
-            try{
-                s.acquire();
-                System.out.println("Il treno si trova sulla tratta "+stPartenza+"-"+stArrivo);
-                System.out.println("L'arrivo è previsto alla stazione "+stArrivo);
-                s.release();
+    public void run() {
+        while (true) {
+            try {
+                stazionePartenza.acquire();
+                System.out.println("La partenza è prevista dalla stazione " + stPartenza);
+                stazionePartenza.release();
+                if (scambio != null) {
+                    scambio.acquire();
+                    stazionePartenza.acquire();
+                    System.out.println("Il treno si trova sulla tratta " + stPartenza + "-" + stArrivo + "(passaggio allo scambio)");
+                    stazionePartenza.release();
+                    Thread.sleep(1000); // simulazione del tempo di passaggio allo scambio
+                    stazioneArrivo.acquire();
+                    System.out.println("Il treno si trova sulla tratta " + stPartenza + "-" + stArrivo + "(passaggio allo scambio)");
+                    System.out.println("L'arrivo è previsto alla stazione " + stArrivo);
+                    stazioneArrivo.release();
+                    scambio.release();
+                } else {
+                    stazioneArrivo.acquire();
+                    System.out.println("Il treno si trova sulla tratta " + stPartenza + "-" + stArrivo);
+                    System.out.println("L'arrivo è previsto alla stazione " + stArrivo);
+                    stazioneArrivo.release();
+                }
                 break;
-            }catch(InterruptedException e){
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
